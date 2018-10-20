229d5e83ccc092c09017ba7711c0be12af370c21
allan <allancruz716@gmail.com>
2018-10-20 15:22:34 -0300

    Configuring local notifications

---
+++ src/components/local-notifications.vue
@@ -0,0 +1,33 @@
+<template>
+	<section>
+		<h1>Notificações Locais</h1>
+
+		<p> 
+			Permissão: {{ permission}}
+			 <a href="" @click.prevent="request()" v-if="permission !== 'granted'">Solicitar</a>
+		</p> 
+	</section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			permission: null
+		}
+	},
+
+	methods: {
+		request() {
+			Notification.requestPermission(() =>{
+				this.permission = Notification.permission
+			})
+		}
+	},
+
+
+	mounted() {
+		this.permission = Notification.permission
+	}
+}
+</script>
