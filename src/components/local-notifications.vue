3a0654a5bb4e3ba468821f842aaec12c405bb540
allan <allancruz716@gmail.com>
2018-10-20 22:43:41 -0300

    Triggering notifications

--- src/components/local-notifications.vue
+++ src/components/local-notifications.vue
@@ -6,6 +6,10 @@
 			Permissão: {{ permission}}
 			 <a href="" @click.prevent="request()" v-if="permission !== 'granted'">Solicitar</a>
 		</p> 
+
+
+		<a href="" @click.prevent="firePersistent('Foi um click', 'Este clique foi disparado apartir de um link no qual clicamos.')">Notificação</a>
+
 	</section>
 </template>
 
@@ -18,6 +22,22 @@ export default {
 	},
 
 	methods: {
+		fire(msg, body) {
+			let options = {
+				body: body,
+				icon: 'http://icon-icons.com/icons2/567/PNG/512/eye_icon-icons.com_54399.png'
+			}
+			new Notification(msg, options)
+		},
+		firePersistent(msg, body) {
+			let options = {
+				body: body,
+				icon: 'http://icon-icons.com/icons2/567/PNG/512/eye_icon-icons.com_54399.png'
+			}
+			navegator.serviceWorker.getRegistration()
+				.then(reg => reg.showNotification(msg, options))
+				.catch(err => console.log(err))
+		},
 		request() {
 			Notification.requestPermission(() =>{
 				this.permission = Notification.permission
@@ -28,6 +48,9 @@ export default {
 
 	mounted() {
 		this.permission = Notification.permission
+		setTimeout(() => {
+			this.fire('Foi um mounted', 'Esta notificação foi disparada a partir do lifecilce mounted')
+		}, 3000);
 	}
 }
 </script>
