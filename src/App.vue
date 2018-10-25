4bfb1ee04dff93bd6acdbe75041544bbd72d0ac8
allan <allancruz716@gmail.com>
2018-10-24 21:18:04 -0300

    to implement Bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <b-navbar toggleable="md" type="dark" >
+
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+        <b-navbar-brand href="#">Vue.js PWA</b-navbar-brand>
+
+        <b-collapse is-nav id="nav_collapse">
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+
+            <b-navbar-nav>
+              <b-nav-item href="#">Foreground Detection</b-nav-item>
+              <b-nav-item href="#">Local Notifications</b-nav-item>
+            </b-navbar-nav>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </header>
-    <main>
+    <main class="text-center">
       <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
@@ -28,27 +45,8 @@ body {
   color: #2c3e50;
 }
 
-main {
-  text-align: center;
-  margin-top: 40px;
+.navbar{
+    background: #41b883;
 }
 
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
